3319aa0e8638036b5bca3737b31a3fc6565be32c
joseph-birara <josephbirara92@gmail.com>
2022-10-15 16:14:18 +0300

    bug fixed

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,6 @@
 # creates a file in the /tmp directory
-file { '/tmp/holberton':
-  ensure  => 'file',
+file { '/tmp/school':
+  ensure  => 'present',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
